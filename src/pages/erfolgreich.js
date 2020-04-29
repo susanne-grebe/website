25cb3282b2289667a79a1fefdb36e8477fb3af5e
Calexxxxx <pasm28@hotmail.nl>
2020-04-29 13:16:32 +0200

    Add: Success Page german version

--- src/pages/erfolgreich.js
+++ src/pages/erfolgreich.js
@@ -2,8 +2,8 @@
  * @description CONTACT PAGE GERMAN
  */
 
-import React, { useState } from "react"
-import { graphql } from "gatsby"
+import React from "react"
+import { graphql, Link } from "gatsby"
 import styled from "@emotion/styled"
 import Img from "gatsby-image"
 
@@ -53,120 +53,6 @@ const SectionInner = styled.div`
   }
 `
 
-const Form = styled.form`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  max-width: 540px;
-  margin: 3rem auto;
-  width: 100%;
-  @media (min-width: 768px) {
-    width: 50%;
-  }
-`
-
-const FormGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  justify-content: flex-start;
-  margin: 1rem 0;
-  &[data-visible="false"] {
-    display: none;
-  }
-`
-
-const FormInput = styled.input`
-  font-size: 1rem;
-  border: 1px solid #aa5d00;
-  border-radius: 5px;
-  padding: 0.5rem;
-  &:focus,
-  &:active {
-    outline-color: #aa5d00;
-  }
-  &:-webkit-autofill,
-  &:-webkit-autofill:hover,
-  &:-webkit-autofill:focus,
-  &:-webkit-autofill:active {
-    -webkit-box-shadow: 0 0 0 30px white inset !important;
-  }
-`
-
-const FormTextarea = styled.textarea`
-  font-size: 1rem;
-  border: 1px solid #aa5d00;
-  border-radius: 5px;
-  padding: 0.5rem;
-  &:focus,
-  &:active {
-    outline-color: #aa5d00;
-  }
-`
-
-const FormSelect = styled.select`
-  font-size: 1rem;
-  border: 1px solid #aa5d00;
-  border-radius: 5px;
-  padding: 0.5rem;
-  cursor: pointer;
-  appearance: none;
-  background-color: #fff;
-  background-image: linear-gradient(45deg, transparent 50%, #aa5d00 50%),
-    linear-gradient(135deg, #aa5d00 50%, transparent 50%),
-    linear-gradient(to right, #aa5d00, #aa5d00);
-  background-position: calc(100% - 20px) calc(1em + 2px),
-    calc(100% - 15px) calc(1em + 2px), calc(100% - 2.5em) 0.5em;
-  background-size: 5px 5px, 5px 5px, 1px 1.5em;
-  background-repeat: no-repeat;
-  &:focus,
-  &:active {
-    outline-color: #aa5d00;
-  }
-`
-
-const FormSelectOption = styled.option`
-  padding: 0.5rem;
-  font-size: 1rem;
-`
-
-const FormLabel = styled.label`
-  font-weight: 400;
-  font-size: 1.125rem;
-`
-
-const FormButton = styled.button`
-  font-size: 1rem;
-  padding: 0.5rem;
-  color: #aa5d00;
-  border: 1px solid #aa5d00;
-  border-radius: 5px;
-  text-transform: uppercase;
-  transition: all 0.3s ease-in-out;
-  display: inline-block;
-  width: 150px;
-  background-color: #fff;
-  text-align: center;
-  &:hover,
-  &:focus {
-    background-color: #aa5d00;
-    border-color: #aa5d00;
-    color: #fff;
-  }
-  &:disabled,
-  &[disabled] {
-    color: #404040;
-    border-color: #404040;
-    &:hover,
-    &:focus {
-      background-color: #fff;
-      color: #404040;
-      border-color: #404040;
-    }
-  }
-`
-
 const SectionContactInfo = styled.div`
   display: flex;
   flex-direction: column;
@@ -213,25 +99,56 @@ const SocialProfileItem = styled.li`
   }
 `
 
-const IndexPage = ({ data, location }) => {
-  const [checked, setChecked] = useState(false)
+const SectionSuccessMessage = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 2rem auto;
+  max-width: 540px;
+  @media (min-width: 768px) {
+    width: 50%;
+    padding-left: 2rem;
+  }
+`
 
-  console.log(checked)
+const NavSection = styled.nav`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`
 
+const NavItem = styled(Link)`
+  color: #aa5d00;
+  transition: all 0.3s ease-in-out;
+  padding: 0 1rem;
+  &:hover,
+  &:focus {
+    color: #7b341d;
+  }
+  @media (min-width: 768px) {
+    padding: 0 0.5rem;
+    &:nth-of-type(1) {
+      padding-left: 0;
+    }
+    &:nth-of-type(4) {
+      padding-right: 0;
+    }
+  }
+`
+
+const SuccessPage = ({ data }) => {
   const heroData = {
     nodes: [
       {
-        homePageHeroTitle: "Kontakt aufnehmen",
-        homePageHeroSubtitle: "Haben Sie noch Fragen?",
+        homePageHeroTitle: "Erfolgreich",
+        homePageHeroSubtitle: "Ich habe Ihre Nachricht erhalten.",
         homePageHeroSlogan: "",
         homePageHeroBackgroundImage:
           data.localBusinessDE.nodes[0].seoCompanyImage,
       },
     ],
-  }
-
-  const handleCheckboxChange = () => {
-    setChecked(!checked)
   }
 
   return (
@@ -242,7 +159,7 @@ const IndexPage = ({ data, location }) => {
       <Hero heroData={heroData} />
       <main className="main">
         <Section>
-          <SectionHeader title="Kontakt aufnehmen." />
+          <SectionHeader title="Erfolgreich" />
 
           <SectionInner>
             <SectionContactInfo>
@@ -297,118 +214,23 @@ const IndexPage = ({ data, location }) => {
                 </SocialProfileItem>
               </SocialProfiles>
             </SectionContactInfo>
-            <Form
-              name="contact"
-              method="POST"
-              netlify-honeypot="bot-field"
-              data-netlify="true"
-              action="/pages/erfolgreich"
-            >
-              <FormGroup data-visible="false">
-                <FormLabel htmlFor="botfield">
-                  <h3>Füllen Sie das nicht aus, wenn Sie ein Mensch sind</h3>
-                </FormLabel>
-                <FormInput
-                  name="bot-field"
-                  aria-label="bot field"
-                  id="botfield"
-                />
-              </FormGroup>
-              <FormGroup>
-                <FormLabel htmlFor="name">
-                  <h3>Ihre Name</h3>
-                </FormLabel>
-                <FormInput
-                  id="name"
-                  type="text"
-                  name="name"
-                  placeholder="e.g. Jane Doe"
-                  aria-label="Ihre Name"
-                />
-              </FormGroup>
-              <FormGroup>
-                <FormLabel htmlFor="email">
-                  <h3>Ihre Email</h3>
-                </FormLabel>
-                <FormInput
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="e.g. example@email.com"
-                  aria-label="Ihre email"
-                />
-              </FormGroup>
-              <FormGroup>
-                <FormLabel htmlFor="phone">
-                  <h3>Ihre Rufnummer</h3>
-                </FormLabel>
-                <FormInput
-                  id="phone"
-                  type="tel"
-                  name="phone"
-                  placeholder="e.g. +49 123456789"
-                  aria-label="Ihre Rufnummer"
-                />
-              </FormGroup>
-              <FormGroup>
-                <FormLabel htmlFor="thema">
-                  <h3>Ihr Thema</h3>
-                </FormLabel>
-                <FormSelect name="thema" id="thema">
-                  <FormSelectOption
-                    value="coaching"
-                    aria-label="coaching thema"
-                  >
-                    Coaching
-                  </FormSelectOption>
-                  <FormSelectOption
-                    value="mediation"
-                    aria-label="mediation thema"
-                  >
-                    Mediation
-                  </FormSelectOption>
-                  <FormSelectOption
-                    value="workshops"
-                    aria-label="workshops thema"
-                  >
-                    Workshops
-                  </FormSelectOption>
-                  <FormSelectOption value="other" aria-label="etwas anderes">
-                    Etwas anderes
-                  </FormSelectOption>
-                </FormSelect>
-              </FormGroup>
-              <FormGroup>
-                <FormLabel htmlFor="frage">
-                  <h3>Ihre Frage / Nachricht</h3>
-                </FormLabel>
-                <FormTextarea
-                  id="frage"
-                  name="message"
-                  placeholder="Ihre Frage"
-                  aria-label="Ihre Frage / Nachricht"
-                  rows="5"
-                ></FormTextarea>
-              </FormGroup>
-              <FormGroup>
-                <p>
-                  <input
-                    defaultChecked={checked === true ? "checked" : ""}
-                    onChange={() => handleCheckboxChange()}
-                    type="checkbox"
-                    style={{ marginRight: "1rem" }}
-                  />
-                  Ich habe die{" "}
-                  <a href="/cookie-richtlinie">Cookie-Richtlinie</a> gelesen und
-                  bin damit einverstanden...{" "}
-                </p>
-              </FormGroup>
-              <FormGroup>
-                <FormButton type="submit" disabled={!checked}>
-                  Senden
-                </FormButton>
-              </FormGroup>
-            </Form>
+            <SectionSuccessMessage>
+              <h2>Ihre Nachricht ist erhalten.</h2>
+              <p>
+                Ich werde mein Bestes tun, um innerhalb von 24 Stunden zu
+                antworten.
+              </p>
+              <p>
+                In der Zwischenzeit finden Sie vielleicht eine Antwort in den
+                folgenden Abschnitten.{" "}
+              </p>
+              <NavSection>
+                <NavItem to="/coaching-im-raum-aachen">Coaching</NavItem>
+                <NavItem to="/mediation-im-raum-aachen">Mediation</NavItem>
+                <NavItem to="/seminare-im-raum-aachen">Seminare</NavItem>
+                <NavItem to="/blog">Blog</NavItem>
+              </NavSection>
+            </SectionSuccessMessage>
           </SectionInner>
         </Section>
         <Section
@@ -437,7 +259,7 @@ const IndexPage = ({ data, location }) => {
   )
 }
 
-export default IndexPage
+export default SuccessPage
 
 export const LayoutQuery = graphql`
   query {
