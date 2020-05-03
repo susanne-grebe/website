cf2c7a9deb2e41112c62baef7269a80bfd79c3fc
Calexxxxx <pasm28@hotmail.nl>
2020-05-03 12:15:14 +0200

    Fix: content width tablet screens

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -31,7 +31,10 @@ const Wrapper = styled.div`
   max-width: 540px;
   margin: 0 auto;
   @media (min-width: 768px) {
-    max-width: 992px;
+    max-width: 720px;
+    main {
+      max-width: 720px;
+    }
   }
   @media (min-width: 992px) {
     max-width: 1280px;
